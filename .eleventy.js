64b1a92fc6d89bc6180a42a1d1d2906a0b11091c
James Belchamber <james@belchamber.com>
2021-02-11 12:19:29 +0000

    Limiting posts to specific tag, updating dependencies

--- .eleventy.js
+++ .eleventy.js
@@ -46,36 +46,13 @@ module.exports = function(config) {
   // Don't ignore the same files ignored in the git repo
   config.setUseGitIgnore(false);
 
-  // Get all pages, called 'docs' to prevent
-  // conflicting the eleventy page object
-  config.addCollection("docs", async function(collection) {
-    collection = await api.pages
-      .browse({
-        include: "authors",
-        limit: "all"
-      })
-      .catch(err => {
-        console.error(err);
-      });
-
-    collection.map(doc => {
-      doc.url = stripDomain(doc.url);
-      doc.primary_author.url = stripDomain(doc.primary_author.url);
-
-      // Convert publish date into a Date object
-      doc.published_at = new Date(doc.published_at);
-      return doc;
-    });
-
-    return collection;
-  });
-
   // Get all posts
   config.addCollection("posts", async function(collection) {
     collection = await api.posts
       .browse({
         include: "tags,authors",
-        limit: "all"
+        limit: "all",
+        filter: 'tag:justLiberals'
       })
       .catch(err => {
         console.error(err);
@@ -124,7 +101,8 @@ module.exports = function(config) {
     const posts = await api.posts
       .browse({
         include: "authors",
-        limit: "all"
+        limit: "all",
+        filter: 'tag:justLiberals'
       })
       .catch(err => {
         console.error(err);
@@ -146,41 +124,6 @@ module.exports = function(config) {
         author.website_short = author.website.replace(/https?:\/\/w*\.?/, '');
         author.website_short = author.website_short.replace(/\/$/, '');
       }
-    });
-
-    return collection;
-  });
-
-  // Get all tags
-  config.addCollection("tags", async function(collection) {
-    collection = await api.tags
-      .browse({
-        include: "count.posts",
-        limit: "all"
-      })
-      .catch(err => {
-        console.error(err);
-      });
-
-    // Get all posts with their tags attached
-    const posts = await api.posts
-      .browse({
-        include: "tags,authors",
-        limit: "all"
-      })
-      .catch(err => {
-        console.error(err);
-      });
-
-    // Attach posts to their respective tags
-    collection.forEach(async tag => {
-      const taggedPosts = posts.filter(post => {
-        post.url = stripDomain(post.url);
-        return post.primary_tag && post.primary_tag.slug === tag.slug;
-      });
-      if (taggedPosts.length) tag.posts = taggedPosts;
-
-      tag.url = stripDomain(tag.url);
     });
 
     return collection;
